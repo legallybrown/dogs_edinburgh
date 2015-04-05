f1846bf8bc6af326e470cd7e93bbc59ea22c43a3
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2015-04-05 04:20:00 +0100

    render content in info window

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -6,7 +6,19 @@ class Marker < ActiveRecord::Base
     results = []
     all_markers = Marker.all
     all_markers.each do |marker|
-      location = [marker.name, marker.lat, marker.lng]
+      location = [
+        marker.name, 
+        marker.lat, 
+        marker.lng, 
+        marker.business_type, 
+        marker.url, 
+        marker.opening_time.strftime("%H %M"), 
+        marker.closing_time.strftime("%H %M"),
+        marker.rating,
+        marker.postcode,
+        marker.street_name,
+        marker.building_number
+      ]
       results.push(location)
     end
     results.to_json
