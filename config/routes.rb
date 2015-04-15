070d412fd6443b17ecfccfa82dba74200ba94245
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2015-04-16 00:38:04 +0100

    root to home show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#show', defaults: { format: :json }
+  root 'home#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
+  
   get 'admin' => 'admin#main'
 
 
